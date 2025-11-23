29e1b4744bfdfde89c49c28eb1b3abac9fd8992b
Daniel77688 <daniel77688@gmail.com>
2025-11-23 13:01:11 +0100

    Rutas

---
+++ config.py
@@ -0,0 +1,8 @@
+import os 
+from pathlib import Path
+
+# Rutas del proyecto
+BASE_DIR = Path(__file__).parent
+CSV_DIR = BASE_DIR / "data" / "csv"
+STATIC_DIR = BASE_DIR / "static"
+TEMPLATES_DIR = BASE_DIR / "templates"
